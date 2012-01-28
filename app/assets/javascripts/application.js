b9e0dc9124f8c23354751397e449178c85ebdab1
Luke Smith <smithmr8@hotmail.co.uk>
2012-01-28 22:38:44 +0000

    Final changes for tonight

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,6 @@
 $(function() {
-
-	$("#newCampaign").click(function(){
-		console.log("Click: New campaign");
-	});
 	
 	$(".formField").change(function(){
-		console.log($(this).attr("name") + " - " + $(this).is(':checked'));
 		$(".form"+$(this).attr("name")).toggle();
 	});
 
