580954a6241693e5ff4e6067298e58639b5a4a71
Luke Smith <smithmr8@hotmail.co.uk>
2012-02-13 21:50:51 +0000

    JS File Type Validation.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,36 @@ $(function() {
 		$(".form"+$(this).attr("name")).toggle();
 	});
 
+  $("#submit_new_template").click(function(){
+    var valid = true;
+    var file = $("#templatefile").val();
+    var name = $("#templatename").val();
+    $("#templatefile").removeClass("invalid");
+    $("#templatename").removeClass("invalid");
+    
+    if ((!valid_file_type()) || (file == "")){
+      $("#templatefile").addClass("invalid");
+      valid = false;
+    }
+    
+    if (name == ""){
+      $("#templatename").addClass("invalid");
+      valid = false;
+    }
+    
+    return valid;
+  });
+
 });
+
+function valid_file_type(){
+  var valid = false;
+  var file = $("#templatefile").val();
+  var splitfile = file.split(".");
+  if (splitfile.length == 2){
+    if (splitfile[1].toLowerCase() == "html"){
+      valid = true;
+    }   
+  }
+  return valid;
+}
